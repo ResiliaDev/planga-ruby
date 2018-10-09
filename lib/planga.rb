c0d908ef9dd7788c023f9888d1ecfe2213f8b463
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-10-09 20:34:05 +0200

    Changelog

--- lib/planga.rb
+++ lib/planga.rb
@@ -2,11 +2,35 @@ require 'securerandom'
 require 'jose'
 require 'json'
 
+# This class allows you to create a snippet of HTML/JS that,
+# if included in a webpage, lets the visitor of that webpage
+# connect with the Planga Chat Server and start chatting.
 class Planga
     attr_accessor :public_api_id, :private_api_key, :conversation_id,
-        :current_user_id, :current_user_name, :container_id, :remote_host
+        :current_user_id, :current_user_name, :container_id, :remote_host, :debug
 
     
+    # The following configuration options are available:
+    #
+    # Required are:
+    #
+    # * public_api_id: The public API ID that can be found in the Planga Dashboard.
+    # * private_api_key: The private API key that can be found in the Planga Dashboard.
+    # * conversation_id: The identifier that uniquely identifies the single conversation
+    #     that the user can connect with in this chat. Examples would be "general", "product/1234" or "private/123/543".
+    # * current_user_id: The internal ID your application uses, which uniquely identifies
+    #     the user currently using your application.
+    # * current_user_name: The name of this user. This name is shown in the chat interface
+    #     next to the typed messages
+    #
+    # Optional are:
+    # 
+    # * remote_host: This can point to another host, if you are hosting your own instance of Planga.
+    #     It defaults to the URL of Planga's main chat server. (`//chat.planga.io`)
+    # * container_id: If you want a custom HTML ID attribute specified to the created HTML element, 
+    #    you can enter it here.
+    # * debug: (defaults to `false`).
+    #
     def initialize(**conf)
         @public_api_id = conf[:public_api_id]
         @private_api_key = conf[:private_api_key]
@@ -15,6 +39,7 @@ class Planga
         @current_user_name = conf[:current_user_name]
         @container_id = conf[:container_id]
         @remote_host = conf[:remote_host] || "//chat.planga.io"
+	@debug = conf[:debug] || false
 
         if not container_id
             @container_id = "planga-chat-" + SecureRandom.hex
@@ -22,6 +47,7 @@ class Planga
     end
     
 
+    # Creates a full-fledged HTML snippet that includes Planga in your page.
     def chat_snippet
         return %{
             <script type=\"text/javascript\" src=\"#{self.remote_host}/js/js_snippet.js\"></script>
@@ -29,16 +55,20 @@ class Planga
             <script type=\"text/javascript\">
                new Planga(document.getElementById(\"#{self.container_id}\"), \{
                    public_api_id: \"#{self.public_api_id}\",
-                   encrypted_options: \"#{encrypt_options()}\",
+                   encrypted_options: \"#{encrypted_options()}\",
                    socket_location: \"#{self.remote_host}/socket\",
+                   debug: #{self.debug},
                \});
             </script>
         }
     end
     
 
-    private
-    def encrypt_options
+    # Returns the encrypted configuration.
+    #
+    # This function is useful if (and only if) you do not want to use the normal chat snippet,
+    # but want to completely customize how Planga loads (so you want to create it manually).
+    def encrypted_options
         key = JOSE::JWK.from({"k" => self.private_api_key, "kty" => "oct"})
 
         payload = {
--- planga.gemspec
+++ planga.gemspec
@@ -1,13 +1,14 @@
 Gem::Specification.new do |s|
   s.name         = 'planga'
-  s.version      = '0.0.4'
-  s.date         = '2018-09-13'
-  s.summary      = "Planga"
-  s.description  = "Wrapper for interacting with the Planga chat server."
+  s.version      = '0.5.0'
+  s.date         = '2018-10-09'
+  s.summary      = "Ruby Wrapper for the Planga Chat Service"
+  s.description  = "Ruby Wrapper for interacting with the Planga Chat Service."
   s.authors      = ["Wiebe Marten Wijnja", "Jeroen Hoekstra"]
   s.files        = Dir['README.md', 'VERSION', 'Gemfile', 'Rakefile', '{bin,lib,config,vendor}/**/*']
   s.require_path = 'lib'
-  s.homepage     = 'https://github.com/ResiliaDev/Planga'
+  s.homepage     = 'https://github.com/ResiliaDev/planga-ruby'
+  s.email      = 'contact@planga.io'
   s.license      = 'MIT'
 
   s.add_dependency('jose', '~> 1.1')
